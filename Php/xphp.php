1fcfab1ebf3f95f0ac9938d3527bf4c21098b147
Juan David <juandacoga12@gmail.com>
2021-05-13 19:32:44 -0500

    Se desarrollo el index y parte visual de la prueba tecnica

---
+++ Php/xphp.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>La x</title>
+</head>
+<body>
+    <h1>Resultados del pedido</h1>
+    
+    <?php
+        echo '<p>Pedido procesado a las ';
+        echo date('H:i, jS F');
+        echo '</p>';
+    ?>
+</body>
+</html>
